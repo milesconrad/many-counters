ff426dc0854111b432a3355a9992a3df419f5af2
Miles Conrad <72412676+milesconrad@users.noreply.github.com>
2022-04-26 23:02:07 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const TABLE_WIDTH = 158;
+const TABLE_HEIGHT = 41;
+const table = document.getElementById("chaos")
+
+let grid = new Array(TABLE_HEIGHT)
+for (let i = 0; i < grid.length; i++) {
+    grid[i] = new Array(TABLE_WIDTH)
+}
+
+let values = new Array(grid.length)
+for (let i = 0; i < grid.length; i++) {
+    values[i] = new Array(grid[0].length)
+    for (let j = 0; j < grid[i].length; j++) {
+        values[i][j] = 0
+    }
+}
+
+for (let i = 0; i < grid.length; i++) {
+    const row = document.createElement("tr")
+    table.appendChild(row)
+
+    for (let j = 0; j < grid[i].length; j++) {
+        grid[i][j] = document.createElement("td")
+        row.appendChild(grid[i][j])
+        grid[i][j].innerHTML = values[i][j].toString()
+    }
+}
+
+function increment() {
+    let i = Math.floor(Math.random() * TABLE_HEIGHT)
+    let j = Math.floor(Math.random() * TABLE_WIDTH)
+    
+    values[i][j]++
+    grid[i][j].innerHTML = values[i][j].toString()
+}
+
+for (let i = 0; i < 200; i++) {
+    setInterval(increment, 1)
+}
+
+document.title = (TABLE_HEIGHT * TABLE_WIDTH).toString() + " Counters"
